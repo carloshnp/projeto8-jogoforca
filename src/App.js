6df97483be6b14f78c28ce0846ba5115bb04f774
carloshnp <carloshnpwork@gmail.com>
2022-09-22 18:49:41 -0300

    DEV-1 componentes criados

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Jogo from "./Jogo"
+import Letras from "./Letras"
+import Chute from "./Chute"
+
+export default function App() {
+    return (
+        <>
+            <Jogo/>
+            <Letras/>
+            <Chute/>
+        </>
+    )
+}
