f9c2fe2f651165cc5e821e9ac0b26bde84516e4f
Carlos Pereira <75504798+carloshnp@users.noreply.github.com>
2022-09-25 14:06:22 -0300

    teste sortear palavra

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App() {
     const contadorForca = 0;
     
     function selecionarPalavra() {
-      alert(Math.floor(Math.random()*10));
+      alert(Math.floor(Math.random()*palavras.length));
       {/* const palavra = palavras.[Math.floor(Math.random() * palavras.length)];*/}
       alert('palavra');
     }
